01c5332297e720d304311936f5a78994390d2bf9
aqian853 <alyssaqianry@gmail.com>
2025-07-10 09:52:08 -0400

    Update GMM_LV1.R

--- code/GMM2/GMM_LV1.R
+++ code/GMM2/GMM_LV1.R
@@ -14,7 +14,7 @@ library(ggalluvial)
 library(reshape2)
 
 PEPP_2024_12_12_IDmatched <- read_sav(
-  "/Users/aq/Documents/PePP_Project/PEPP_2024-12-12-IDmatched.sav")
+  "PEPP_2024-12-12-IDmatched.sav")
 
 #Total CDSS Scores
 PEPP_2024_12_12_IDmatched <- PEPP_2024_12_12_IDmatched %>%
